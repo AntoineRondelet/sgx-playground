45bd7c359f6919d0e4771e30a16d074bb53edaad
Antoine Rondelet <rondelet.antoine@gmail.com>
2018-05-08 09:18:24 +0100

    Added comments and fixed bad include in main.c

--- go-enclave/Enclave/test/main.c
+++ go-enclave/Enclave/test/main.c
@@ -1,15 +1,17 @@
-#include "libcalc.h"
+#include "calc.h"
 #include <stdio.h>
 
 int addition(int a, int b) {
     GoInt result;
+
+    // The logic of the addition is implemented in Golang
     result = Addition((GoInt)a, (GoInt)b);
     int c_type_result = (int)result;
     return c_type_result;
 }
 
 int main() {
-  printf("This is a C Application.\n");
+  printf("This is a C application calling some Go code\n");
   int res = addition(1, 100000);
   printf("Result: %d\n", res);
   return 0;
